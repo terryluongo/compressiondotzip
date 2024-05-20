61c803a790b7bc788272689cc0e02b139f853017
jiffylesica <153464614+jiffylesica@users.noreply.github.com>
2024-05-20 04:01:23 -0400

    Update saliency.py

--- saliency.py
+++ saliency.py
@@ -34,7 +34,7 @@ def LoG_view(img, sigma, rToG = True):
     plt.imshow(filtered, cmap='gray')
     plt.title(f"Edge detected image with sigma {sigma}")
 
-def mean_LoG_view(img, sigma, rToG = True):
+def mean_LoG_view(img, sigma = 5, rToG = True):
     """
     Displays MEAN 8x8 block LoG view of an image. If rToG is not False, will assume original image is RGB
     and convert to grayscale.
@@ -49,9 +49,13 @@ def mean_LoG_view(img, sigma, rToG = True):
         for j in range(0, imsize[1], 8):
             mean_array[i:(i+8),j:(j+8)] = np.mean(LoG[i:(i+8), j:(j+8)])
 
-    plt.figure()
-    plt.imshow(mean_array, cmap='gray')
-    plt.title(f"8x8 mean blocks of image LoG with sigma = {sigma}")
+    sliced = mean_array[::8, ::8]
+
+    rang = sliced.max() - sliced.min()
+    sliced -= sliced.min()
+    sliced /= rang
+    sliced *= 255
+    return sliced.astype(np.uint8)
 
 def std_LoG_view(img, sigma = 5, rToG = True):
     """
@@ -74,4 +78,125 @@ def std_LoG_view(img, sigma = 5, rToG = True):
     sliced -= sliced.min()
     sliced /= rang
     sliced *= 255
+    
+    return sliced.astype(np.uint8)
+
+
+
+# Saliency Maps
+
+def create_saliency(img, mode = 'FG'):
+    """
+    Creates saliency map of image using cv2 Spectral Residual or Fine Grained
+    Method (depending on mode parameter). If no mode passed, Fine Grained used.
+
+    returns saliency map
+    """
+    if mode == 'SR':
+        saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+        (success, saliencyMap) = saliency.computeSaliency(img)
+        
+        return saliencyMap
+    elif mode == 'FG':
+        saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+        (success, saliencyMap) = saliency.computeSaliency(img)
+     
+        return saliencyMap
+    
+# Functions to calculate metrics from saliency maps, and hold in 8x8 blocks
+
+def mean_saliency_view(img, mode = 'FG'):
+    """
+    Calculates mean of saliency map luminance values in 8x8 blocks across image.
+    """
+    salMap = create_saliency(img, mode)
+
+    imsize = salMap.shape
+    mean_array = np.zeros(imsize)
+
+    # Do 8x8 DCT on image (in-place)
+    for i in range(0, imsize[0], 8):
+        for j in range(0, imsize[1], 8):
+            mean_array[i:(i+8),j:(j+8)] = np.mean(salMap[i:(i+8), j:(j+8)])
+    
+    sliced = mean_array[::8, ::8]
+
+    rang = sliced.max() - sliced.min()
+    sliced -= sliced.min()
+    sliced /= rang
+    sliced *= 255
+
+    return sliced.astype(np.uint8)
+
+def std_saliency_view(img, mode = 'FG'):
+    """
+    Calculates standard deviation of saliency map luminance values in 8x8 blocks across image.
+    """
+    salMap = create_saliency(img, mode)
+
+    imsize = salMap.shape
+    std_array = np.zeros(imsize)
+
+    # Do 8x8 DCT on image (in-place)
+    for i in range(0, imsize[0], 8):
+        for j in range(0, imsize[1], 8):
+            std_array[i:(i+8),j:(j+8)] = np.std(salMap[i:(i+8), j:(j+8)])
+    
+    
+    sliced = std_array[::8, ::8]
+
+    rang = sliced.max() - sliced.min()
+    sliced -= sliced.min()
+    sliced /= rang
+    sliced *= 255
+
+    return sliced.astype(np.uint8)
+
+# DCT views
+
+def mean_dct_view(img, rToG = True):
+    """
+    Takes an image as input, converts to grayscale from RGB (unless rToG is False)
+    And creates array which holds mean of DCT of image split into 8x8 blocks.
+    """
+    if rToG:
+        img = skolor.rgb2gray(img)
+
+    imsize = img.shape
+    mean_array = np.zeros(imsize)
+
+    # Do 8x8 DCT on image (in-place)
+    for i in range(0, imsize[0], 8):
+        for j in range(0, imsize[1], 8):
+            mean_array[i:(i+8),j:(j+8)] = np.mean(scipy.fft.dctn(img[i:(i+8), j:(j+8)]))
+    
+    sliced = mean_array[::8, ::8]
+
+    rang = sliced.max() - sliced.min()
+    sliced -= sliced.min()
+    sliced /= rang
+    sliced *= 255
+
+    return sliced.astype(np.uint8)
+
+
+def std_dct_view(img, rToG = True):
+    if rToG:
+        img = skolor.rgb2gray(img)
+
+    imsize = img.shape
+    std_array = np.zeros(imsize)
+
+    # Do 8x8 DCT on image (in-place)
+    for i in range(0, imsize[0], 8):
+        for j in range(0, imsize[1], 8):
+            std_array[i:(i+8),j:(j+8)] = np.std(scipy.fft.dctn(img[i:(i+8), j:(j+8)]))
+    
+    sliced = std_array[::8, ::8]
+
+    rang = sliced.max() - sliced.min()
+    sliced -= sliced.min()
+    sliced /= rang
+    sliced *= 255
+
     return sliced.astype(np.uint8)
